b847d0d2c9eefd2d0ff9841b00dde222ba1753dc
Lee Nelson <lee@brewhouse.io>
2016-05-23 20:16:24 -0700

    adds a compiled css

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,29 @@
-var nesting       = require('postcss-nested');
-var variables     = require('postcss-simple-vars');
-var mixins        = require('postcss-mixins');
-var autoprefixer  = require('autoprefixer');
-var postcssImport = require('postcss-import');
-
+const path              = require('path');
+const nesting           = require('postcss-nested');
+const variables         = require('postcss-simple-vars');
+const mixins            = require('postcss-mixins');
+const autoprefixer      = require('autoprefixer');
+const postcssImport     = require('postcss-import');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: "./index.js",
-  output: {
-    path: __dirname,
-    filename: "bundle.js"
-  },
   module: {
     loaders: [
       {
         test: /\.pcss$/,
-        loader: "style-loader!css-loader!postcss-loader"
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!postcss-loader?')")
       }
     ]
   },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, './build'),
+    publicPath: '/build'
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ],
   postcss: function (webpack) {
     return [
       postcssImport({
@@ -29,6 +34,10 @@ module.exports = {
       nesting,
       autoprefixer
     ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.pcss'],
+    root: [path.join(__dirname, './stylesheets')]
   },
   watch: true,
   devtool: 'source-map'
